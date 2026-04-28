a6df2c6028e231ae44f39746bdb6dd597464c593
Mauricio Lomelin <maulomelin@gmail.com>
2026-04-28 15:34:18 -0400

    Add VLC media player to setup manifest

--- lib/manifests/100--general-utilities.zsh
+++ lib/manifests/100--general-utilities.zsh
@@ -6,22 +6,26 @@
 # -----------------------------------------------------------------------------
 
 function () {
-    # --------------------------------------
-    log::info_header "Setup: Wine (run Windows applications on macOS)"
-    #   - https://www.winehq.org/
-    # --------------------------------------
-
     # Check if Homebrew is installed.
     if ! command -v brew &> /dev/null ; then
-        log::error "Error installing Wine: Homebrew is not installed."
+        log::error "Error installing General Utilities: Homebrew is not installed."
         log::error "Please install Homebrew and try again."
         return 1
     fi
 
-    # Install Wine.
-    log::warning "Installation of Wine is disabled because it does not pass the macOS Gatekeeper check !"
+    # --------------------------------------
+    log::info_header "Setup: Wine (run Windows applications on macOS)"
+    #   - https://www.winehq.org/
+    # --------------------------------------
+    # brew install --cask wine-stable
+    log::info "Installation of Wine is disabled because it does not pass the macOS Gatekeeper check !"
     log::info "To install Wine manually with Homebrew:"
     log::info "  $ brew install --cask wine-stable"
-    # brew install --cask wine-stable
+
+    # --------------------------------------
+    log::info_header "Setup: VLC media player"
+    #   - https://www.videolan.org/vlc/
+    # --------------------------------------
+    brew install --cask vlc
 
 } || return 1
